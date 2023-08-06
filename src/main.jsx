a0ac1e6370fd254b5b476202190699c7d73e49d6
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-08-06 18:58:42 +0600

    Setup css variables and inject them into tailwind config.

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import '@fontsource/dm-sans/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1 className='text-2xl font-semibold'>Hello World</h1>
+    <h1 className='text-2xl font-semibold text-accent'>Hello World</h1>
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
 
+// imported in src/main.js
 const extendedFontFamily = {
   'sans': ['DM Sans', ...defaultTheme.fontFamily.sans],
+}
+
+// Curated from src/index.css@line:5
+const extendedColors = {
+  'theme': 'var(--theme-color)',
+  'background': 'var(--background-color)',
+  'accent': 'var(--accent-color)',
+  'header': 'var(--header-color)',
+  'body': 'var(--body-color)',
+  'link': 'var(--link-color)',
 }
 
 export default {
@@ -14,6 +25,7 @@ export default {
   theme: {
     extend: {
       fontFamily: extendedFontFamily,
+      colors: extendedColors,
     },
   },
   plugins: [],
