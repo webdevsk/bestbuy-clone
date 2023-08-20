ca8d7aafb2932d4f2a40c9df91b1ba45ec8995f6
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-08-20 23:49:21 +0600

    Done with price filter Ui

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -12,7 +12,7 @@ import {
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
-function Icon({ id, open }) {
+const Icon = ({ id, open }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -32,8 +32,12 @@ function Icon({ id, open }) {
   )
 }
 
+const createUnique = (arr, key) => [
+  ...new Map(arr.map((item) => [item[key], item])).keys(),
+]
+
 const Filters = ({ products }) => {
-  const [open, setOpen] = useState(1)
+  const [open, setOpen] = useState(3)
   const handleOpen = (value) => setOpen(open === value ? 0 : value)
 
   return (
@@ -45,16 +49,72 @@ const Filters = ({ products }) => {
         >
           Category
         </AccordionHeader>
-        <AccordionBody>
-          <ul className="flex flex-col gap-2 antialiased">
-            {products.map(({ category }) => (
-              <Link to={"#"} key="product.name">
-                <Typography className="capitalize text-black hover:underline">
-                  {category}
-                </Typography>
+        <AccordionBody className="py-0 pb-4 capitalize text-inherit">
+          <ul className="flex flex-col gap-2">
+            {createUnique(products, "category").map((category) => (
+              <Link to={"/" + category} key="product.id">
+                <Typography className="hover:underline">{category}</Typography>
               </Link>
             ))}
           </ul>
+        </AccordionBody>
+      </Accordion>
+
+      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+        <AccordionHeader
+          className="text-md group border-none text-black hover:text-theme"
+          onClick={() => handleOpen(2)}
+        >
+          Brands
+        </AccordionHeader>
+        <AccordionBody className="py-0 pb-4 capitalize text-inherit">
+          <ul className="flex flex-col gap-2">
+            {createUnique(products, "brand").map((brand) => (
+              <Link to={"/" + brand} key="product.id">
+                <Typography className="hover:underline">{brand}</Typography>
+              </Link>
+            ))}
+          </ul>
+        </AccordionBody>
+      </Accordion>
+
+      <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
+        <AccordionHeader
+          className="text-md group border-none text-black hover:text-theme"
+          onClick={() => handleOpen(3)}
+        >
+          Price
+        </AccordionHeader>
+        <AccordionBody className="py-0 pb-4 text-inherit">
+          <div className="flex flex-col gap-2 pl-1 pr-4">
+            <div className="flex items-center gap-2">
+              <div className="w-1 grow">
+                <Typography>Min</Typography>
+                <input
+                  type="text"
+                  placeholder="$"
+                  className="max-w-full rounded-sm border border-gray-600 p-4 ring-4 ring-transparent ring-offset-1 ring-offset-transparent transition-[box-shadow] focus-visible:border-blue-900 focus-visible:outline-none focus-visible:ring-blue-100 focus-visible:ring-offset-blue-900"
+                  onKeyDown={(event) =>
+                    !/[0-9]|Backspace/.test(event.key) && event.preventDefault()
+                  }
+                ></input>
+              </div>
+
+              <div className="pt-5">-</div>
+
+              <div className="w-1 grow">
+                <Typography>Max</Typography>
+                <input
+                  type="text"
+                  placeholder="$"
+                  className="max-w-full rounded-sm border border-gray-600 p-4 ring-4 ring-transparent ring-offset-1 ring-offset-transparent transition-[box-shadow] focus-visible:border-blue-900 focus-visible:outline-none focus-visible:ring-blue-100 focus-visible:ring-offset-blue-900"
+                  onKeyDown={(event) =>
+                    !/[0-9]|Backspace/.test(event.key) && event.preventDefault()
+                  }
+                ></input>
+              </div>
+            </div>
+          </div>
         </AccordionBody>
       </Accordion>
     </>
--- src/components/ProductsGallery.jsx
+++ src/components/ProductsGallery.jsx
@@ -7,11 +7,11 @@ const ProductsGallery = () => {
   return (
     <>
       <section>
-        <div className="container flex divide-x border-b">
-          <div className="hidden xl:flex xl:w-1/6">
+        <div className="container flex divide-x border-b xl:px-0">
+          <div className="hidden xl:block xl:w-1/6">
             <Filters products={products} />
           </div>
-          <div className="w-1 grow">Hello</div>
+          <div className="w-1 grow xl:pl-6 xl:pt-6">Hello</div>
         </div>
       </section>
     </>
