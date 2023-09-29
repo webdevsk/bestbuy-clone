1d42fc28a575bd1c07bed840f65895372cc3cffd
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-09-29 19:44:51 +0600

    Directory management and cleanup

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,66 @@
+import { Typography } from "@material-tailwind/react"
+import { FloatElement, FloatHandler, FloatMenu } from "./ui/FloatMenu"
+import { FloatingOverlay, FloatingPortal } from "@floating-ui/react"
+const BurgerMenu = ({ mainMenu, topMiniMenu }) => {
+  return (
+    <FloatMenu
+      click
+      dismiss
+      transition
+      shift
+      offset={8}
+      size
+      arrow={{ fill: "white", width: 20, height: 10 }}
+      placement="bottom"
+    >
+      <FloatHandler className="group relative">
+        <BurgerMenuBtn />
+      </FloatHandler>
+
+      <FloatElement className="w-screen">
+        <FloatingPortal>
+          <FloatingOverlay lockScroll />
+        </FloatingPortal>
+        <div className=" h-full overflow-y-auto border border-t-transparent bg-gray-100 text-body">
+          <ul className="divide-y">
+            {mainMenu?.map((menu) => (
+              <li key={menu.id}>
+                <button className="pointer-events-auto block w-full bg-white p-3 text-start">
+                  <Typography className="text-base">{menu.label}</Typography>
+                </button>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {topMiniMenu?.map((menu) => (
+              <li key={menu.id}>
+                <a href="#" className="block p-3">
+                  <Typography className="text-base">{menu.label}</Typography>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </FloatElement>
+    </FloatMenu>
+  )
+}
+
+export default BurgerMenu
+
+const BurgerMenuBtn = (props) => (
+  <div {...props} className="[--_stroke-height:0.2em] [--_stroke-width:1.5em]">
+    <div className="relative flex transform overflow-hidden transition-all duration-200">
+      <div className="flex h-5  w-[--_stroke-width] origin-center transform flex-col gap-1 overflow-hidden transition-all duration-300">
+        <div className="h-[--_stroke-height] w-[--_stroke-width] origin-left transform rounded bg-white transition-all duration-300 group-open:translate-x-10"></div>
+        <div className="h-[--_stroke-height] w-[--_stroke-width] transform rounded bg-white transition-all delay-75 duration-300 group-open:translate-x-10"></div>
+        <div className="h-[--_stroke-height] w-[--_stroke-width] origin-left transform rounded bg-white transition-all delay-150 duration-300 group-open:translate-x-10"></div>
+
+        <div className="absolute top-2.5 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 group-open:w-12 group-open:translate-x-0 [&>*]:h-[--_stroke-height] [&>*]:w-[--_stroke-width]">
+          <div className="absolute h-[--_stroke-height] w-[--_stroke-width] rotate-0 transform rounded bg-white transition-all delay-300 duration-500 group-open:rotate-45"></div>
+          <div className="absolute h-[--_stroke-height] w-[--_stroke-width] -rotate-0 transform rounded bg-white transition-all delay-300 duration-500 group-open:-rotate-45"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import { Button, Typography } from "@material-tailwind/react"
 import { Link } from "react-router-dom"
 import SearchBar from "./SearchBar"
 import { useEffect, useState } from "react"
-import IconDownLine from "./ui/IconDownLine"
 import createUnique from "../assets/createUnique"
 import { response } from "../assets/disposable"
-import { FloatHandler, FloatElement, FloatMenu } from "./ui/FloatMenu"
+import { FloatElement, FloatHandler, FloatMenu } from "./ui/FloatMenu"
+import IconDownLine from "./ui/IconDownLine"
 import { FloatingOverlay, FloatingPortal } from "@floating-ui/react"
 
 const mainMenu = [
@@ -61,33 +61,9 @@ const Header = () => {
     <div id="header" className="relative z-50">
       <section className="mb-0 bg-theme py-4 text-white">
         <div className="container flex flex-wrap items-center gap-x-4 gap-y-4">
-          <div className="hidden w-full flex-wrap justify-end gap-3 lg:flex">
-            {topMiniMenu.map((menu) => (
-              <Link key={menu.id} to={menu.link} className="hover:underline">
-                <Typography variant="small">{menu.label}</Typography>
-              </Link>
-            ))}
-          </div>
+          <TopMiniMenuDesktop topMiniMenu={topMiniMenu} />
 
-          <div className="-mt-2">
-            <Link to="/" className="flex items-end gap-1">
-              <img src="src/assets/images/logo.png" alt="" width="48" />
-              <div className="-mb-1">
-                <Typography
-                  variant="h3"
-                  className="font-serif leading-none lg:leading-none"
-                >
-                  Best
-                </Typography>
-                <Typography
-                  variant="h3"
-                  className="font-serif leading-none lg:leading-none"
-                >
-                  Buy
-                </Typography>
-              </div>
-            </Link>
-          </div>
+          <SiteLogo />
 
           {!mobile && (
             <div className="w-full lg:w-96">
@@ -95,109 +71,18 @@ const Header = () => {
             </div>
           )}
 
-          <div className="ms-auto">
-            <div className="flex flex-wrap gap-4">
-              <Button
-                variant="text"
-                className="flex flex-wrap items-end gap-1 p-0 text-white hover:bg-transparent hover:text-accent active:bg-transparent"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-7 w-7"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <Typography variant="h6">Account</Typography>
-              </Button>
-
-              <Button
-                variant="text"
-                className="flex flex-wrap items-end gap-1 p-0 text-white hover:bg-transparent hover:text-accent active:bg-transparent"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-7 w-7"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                  />
-                </svg>
-
-                <Typography variant="h6">Cart</Typography>
-              </Button>
-            </div>
-          </div>
+          <HeaderToolBar />
         </div>
       </section>
+
       <section className="mb-0 bg-[#003da6] py-2 text-white">
         <div className="container">
           {mobile && (
             <div className="w-full lg:w-96">
-              <SearchBar mainMenu={mainMenu} />
+              <SearchBar mainMenu={mainMenu} topMiniMenu={topMiniMenu} />
             </div>
           )}
-
-          {!mobile && (
-            <ul className="flex flex-wrap gap-2 py-2">
-              {mainMenu.map((menu) => (
-                <FloatMenu
-                  key={menu.id}
-                  offset={6}
-                  size
-                  shift
-                  transition
-                  click
-                  arrow={{
-                    staticOffset: 10,
-                    width: 20,
-                    height: 10,
-                    fill: "white",
-                  }}
-                  dismiss
-                  role={{ role: "menu" }}
-                >
-                  <FloatHandler className="group flex gap-1 hover:text-accent focus-visible:outline-none">
-                    <Typography variant="h6">{menu.label}</Typography>
-                    <IconDownLine
-                      // open={isOpen}
-                      className="h-4 w-4 group-open:rotate-180"
-                    ></IconDownLine>
-                  </FloatHandler>
-
-                  <FloatElement>
-                    <div
-                      className={`max-h-full w-80  overflow-y-scroll border border-t-transparent bg-white p-4 text-black`}
-                    >
-                      {menu.items.map((item) => (
-                        <li key={item}>{item}</li>
-                      ))}
-                    </div>
-
-                    <FloatingPortal>
-                      <FloatingOverlay
-                        lockScroll
-                        className="pointer-events-none z-30 h-screen w-full bg-black/30"
-                      />
-                    </FloatingPortal>
-                  </FloatElement>
-                </FloatMenu>
-              ))}
-            </ul>
-          )}
+          {!mobile && <MainMenuDesktop mainMenu={mainMenu} />}
         </div>
       </section>
     </div>
@@ -205,3 +90,134 @@ const Header = () => {
 }
 
 export default Header
+
+const SiteLogo = () => (
+  <div className="-mt-2">
+    <Link to="/" className="flex items-end gap-1">
+      <img src="src/assets/images/logo.png" alt="" width="48" />
+      <div className="-mb-1">
+        <Typography
+          variant="h3"
+          className="font-serif leading-none lg:leading-none"
+        >
+          Best
+        </Typography>
+        <Typography
+          variant="h3"
+          className="font-serif leading-none lg:leading-none"
+        >
+          Buy
+        </Typography>
+      </div>
+    </Link>
+  </div>
+)
+
+const HeaderToolBar = () => (
+  <div className="ms-auto">
+    <div className="flex flex-wrap gap-4">
+      <Button
+        variant="text"
+        className="flex flex-wrap items-end gap-1 p-0 text-white hover:bg-transparent hover:text-accent active:bg-transparent"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-7 w-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+        <Typography variant="h6">Account</Typography>
+      </Button>
+
+      <Button
+        variant="text"
+        className="flex flex-wrap items-end gap-1 p-0 text-white hover:bg-transparent hover:text-accent active:bg-transparent"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-7 w-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+
+        <Typography variant="h6">Cart</Typography>
+      </Button>
+    </div>
+  </div>
+)
+
+const MainMenuDesktop = ({ mainMenu }) => {
+  return (
+    <ul className="flex flex-wrap gap-2 py-2">
+      {mainMenu.map((menu) => (
+        <FloatMenu
+          key={menu.id}
+          offset={6}
+          size
+          shift
+          transition
+          click
+          arrow={{
+            staticOffset: 10,
+            width: 20,
+            height: 10,
+            fill: "white",
+          }}
+          dismiss
+          role={{ role: "menu" }}
+        >
+          <FloatHandler className="group flex gap-1 hover:text-accent focus-visible:outline-none">
+            <Typography variant="h6">{menu.label}</Typography>
+            <IconDownLine
+              // open={isOpen}
+              className="h-4 w-4 group-open:rotate-180"
+            ></IconDownLine>
+          </FloatHandler>
+
+          <FloatElement>
+            <div
+              className={`max-h-full w-80  overflow-y-scroll border border-t-transparent bg-white p-4 text-black`}
+            >
+              {menu.items.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </div>
+
+            <FloatingPortal>
+              <FloatingOverlay
+                lockScroll
+                className="pointer-events-none z-30 h-screen w-full bg-black/30"
+              />
+            </FloatingPortal>
+          </FloatElement>
+        </FloatMenu>
+      ))}
+    </ul>
+  )
+}
+
+const TopMiniMenuDesktop = ({ topMiniMenu }) => (
+  <div className="hidden w-full flex-wrap justify-end gap-3 lg:flex">
+    {topMiniMenu.map((menu) => (
+      <Link key={menu.id} to={menu.link} className="hover:underline">
+        <Typography variant="small">{menu.label}</Typography>
+      </Link>
+    ))}
+  </div>
+)
