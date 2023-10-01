e559b6658ca8f4bca9c9686718b20961d494d63b
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-10-01 17:48:49 +0600

    Finalized BurgerMenu mechanism

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,13 +1,44 @@
 import { Typography } from "@material-tailwind/react"
 import { FloatingOverlay, FloatingPortal, size } from "@floating-ui/react"
 import { Float } from "@headlessui-float/react"
-import { Popover } from "@headlessui/react"
-import { Fragment } from "react"
+import { Popover, Transition } from "@headlessui/react"
+import { Fragment, useEffect, useRef, useState } from "react"
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md"
+import { Link } from "react-router-dom"
 
 const BurgerMenu = ({ mainMenu, topMiniMenu }) => {
+  const [show, setShow] = useState(false)
+  const [subMenuIndex, setSubMenuIndex] = useState(null)
+  const popoverRef = useRef(null)
+
+  const handleShow = () => {
+    setShow(!show)
+    setSubMenuIndex(!show && null)
+  }
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (!show) return
+      //Looking for Outside click
+      const rect = popoverRef.current.getBoundingClientRect()
+      if (
+        e.clientX < rect.left ||
+        e.clientX > rect.right ||
+        e.clientY < rect.top ||
+        e.clientY > rect.bottom
+      ) {
+        handleShow()
+      }
+    }
+
+    document.addEventListener("click", handleClick)
+    return () => document.removeEventListener("click", handleClick)
+  }, [show])
+
   return (
     <Popover className="relative grid place-items-center">
       <Float
+        show={show}
         shift
         offset={16}
         arrow={0}
@@ -32,22 +63,61 @@ const BurgerMenu = ({ mainMenu, topMiniMenu }) => {
         originClass="origin-top"
         floatingAs={Fragment}
       >
-        <Popover.Button className="group relative [--_stroke-height:0.2em] [--_stroke-width:1.5em]">
+        <Popover.Button
+          onClick={handleShow}
+          className="group relative [--_stroke-height:0.2em] [--_stroke-width:1.5em]"
+        >
           <BurgerMenuBtn />
         </Popover.Button>
 
-        <Popover.Panel className="pointer-events-none">
+        <Popover.Panel static className="pointer-events-none">
           <Float.Arrow className="absolute h-5 w-5 rotate-45 border border-gray-200 bg-white" />
           <FloatingPortal>
             <FloatingOverlay lockScroll />
           </FloatingPortal>
-          <div className="pointer-events-auto relative h-full overflow-y-scroll  border border-t-transparent bg-gray-100 text-body">
+          <div
+            ref={popoverRef}
+            className="pointer-events-auto relative h-full overflow-x-hidden overflow-y-scroll  border border-t-transparent bg-gray-100 text-body"
+          >
             <ul className="divide-y">
               {mainMenu?.map((menu) => (
                 <li key={menu.id}>
-                  <button className="pointer-events-auto block w-full bg-white p-3 text-start">
+                  <button
+                    className="pointer-events-auto flex w-full items-center justify-between bg-white p-3 text-start"
+                    onClick={() => setSubMenuIndex(menu.id)}
+                  >
                     <Typography className="text-base">{menu.label}</Typography>
+                    {menu.items && menu.items.length !== 0 && (
+                      <MdKeyboardArrowRight className="h-5 w-5 align-middle text-body" />
+                    )}
                   </button>
+                  {menu.items && menu.items.length !== 0 && (
+                    <Transition
+                      show={subMenuIndex === menu.id}
+                      enter="transition ease-in-out duration-300 transform"
+                      enterFrom="translate-x-full"
+                      enterTo="translate-x-0"
+                      leave="transition ease-in-out duration-300 transform"
+                      leaveFrom="translate-x-0"
+                      leaveTo="translate-x-full"
+                      className="absolute inset-0 divide-y overflow-y-auto bg-white"
+                    >
+                      <button
+                        className="sticky top-0 flex w-full items-center gap-2 bg-inherit p-3"
+                        onClick={() => setSubMenuIndex(null)}
+                      >
+                        <MdKeyboardArrowLeft className="h-5 w-5 align-middle text-body" />
+                        <Typography className="text-base font-semibold text-theme">
+                          Back
+                        </Typography>
+                      </button>
+                      {menu.items.map((item) => (
+                        <Link className="block p-3" key={item} to={item}>
+                          {item}
+                        </Link>
+                      ))}
+                    </Transition>
+                  )}
                 </li>
               ))}
             </ul>
