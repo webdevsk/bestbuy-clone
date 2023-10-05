6a2cee0b803fd6b198c00aa2da12f284818ca642
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-10-05 12:59:33 +0600

    Setup cartItemsSlice actions

---
+++ src/features/cartItems/cartItemsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartItemsSlice = createSlice({
+  name: "cartItems",
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      state.push({
+        id: action.payload.id,
+        count: action.payload.count
+      });
+    },
+    removeFromCart: (state, action) => {
+      const itemIndex = state.findIndex(item => item.id == action.payload);
+      state.splice(itemIndex, 1);
+    },
+    increaseCount: (state, action) => {
+      const cartItem = state.find(item => item.id === action.payload.id);
+      cartItem.count += 1;
+    },
+    decreaseCount: (state, action) => {
+      const cartItem = state.find(item => item.id === action.payload.id);
+      cartItem.count -= 1;
+    },
+    setCountByAmount: (state, action) => {
+      const cartItem = state.find(item => item.id === action.payload.id);
+      cartItem.count = action.payload.value;
+    }
+  }
+});
+
+export const { addToCart, removeFromCart, increaseCount, decreaseCount, setCountByAmount } = cartItemsSlice.actions;
+
+export default cartItemsSlice.reducer;
