158c9c54afa185a5c75460e714c1d0a6a49893f3
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-10-11 13:14:43 +0600

    Changed material theme overrides file name and extension

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,30 @@ import "@fontsource/dm-sans/700.css"
 // Supports weights 100-900
 import "@fontsource-variable/inter-tight"
 import { ThemeProvider } from "@material-tailwind/react"
-import { theme } from "./assets/bestbuy-theme"
+import { theme } from "./assets/material-tailwind-overrides"
 import { RouterProvider } from "react-router"
 import { router } from "./router-config"
 import { register } from "swiper/element/bundle"
 import { Provider } from "react-redux"
 import store from "./app/store"
+import { Auth0Provider } from "@auth0/auth0-react"
 
 register()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider value={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </Provider>
+    <Auth0Provider
+      domain={import.meta.env.AUTH0_DOMAIN}
+      clientId={import.meta.env.AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Provider store={store}>
+        <ThemeProvider value={theme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
 )
