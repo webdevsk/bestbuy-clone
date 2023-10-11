01372f10ac0b56d895f59b23bb3b4e329e006319
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-10-11 22:02:12 +0600

    Removed firebase deploy files. Merged RatingBar css with its component

--- src/components/common/RatingBar.jsx
+++ src/components/common/RatingBar.jsx
@@ -1,11 +1,14 @@
 import { memo } from "react"
-import "./ratingBar.css"
 
 const RatingBar = memo(({ rating }) => (
   <div className="rating-bar relative flex w-max">
     <div
-      style={{ width: `${(rating / 5) * 100 ?? 0}%` }}
-      className="rating-bar_overlay absolute left-0 top-0 h-full"
+      style={{
+        width: `${(rating / 5) * 100 ?? 0}%`,
+        backgroundSize: "1rem",
+        backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" stroke="%23fecf0a" fill="%23fecf0a" viewBox="0 0 24 24" stroke-width="1"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" /></svg>')`,
+      }}
+      className="absolute left-0 top-0 h-full"
     ></div>
 
     {Array.from({ length: 5 }).map((_, i) => (
