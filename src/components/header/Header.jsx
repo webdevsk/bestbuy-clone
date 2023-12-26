7a56989309714e30b70c471c44d2bc9ca79ac72f
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-12-26 20:10:38 +0600

    Finalized logic of improved Header component with sticky behavior

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import SearchBar from "./SearchBar"
 import { useEffect, useRef, useState } from "react"
 import MainMenuContext from "../../contexts/MainMenuContext"
 import HeaderMenuContext from "../../contexts/HeaderMenuContext"
-import { Desktop, Mobile } from "../common/ReactResponsive"
 import { HeaderToolBar } from "./HeaderToolBar"
 import { MainMenuDesktop } from "./MainMenuDesktop"
 import { useSelector } from "react-redux"
@@ -12,7 +11,6 @@ import {
   selectProductCategories,
 } from "../../features/api/apiSlice"
 import {
-  AnimatePresence,
   motion,
   useMotionValueEvent,
   useScroll,
@@ -29,6 +27,11 @@ const Header = () => {
   const headerRef = useRef(null)
   const fillerRef = useRef(null)
   const headerOgHeight = useRef(0)
+  // Delaying the appearance of sticky header by 64px
+  // Giving the page enough time to change layout and getting
+  // the shrunken header height
+  const margin = 64
+
   // We can't get the height of the shrunken header until
   // isSticky is true AND it re-renders with the changed layout.
   // So theres a bit of a delay between isSticky getting true
@@ -37,24 +40,31 @@ const Header = () => {
   // drop in tranlateY. going from -0 to -100 (shrunken height)
   // So its better to use a maximum possible shrunken height here
   // to reduce the drop distance. Just use Desktop shrunken mode size
+  // It doesnt have to be accurate. Just a near value would do
   const headerShrunkenHeight = useRef(72) //72 will be -72px initially in translateY
 
   useEffect(() => {
-    // Set filler height after DOM loaded
-    const headerHeight = headerRef.current.getBoundingClientRect().height
-    headerOgHeight.current = headerHeight
-    fillerRef.current.style.height = headerHeight + "px"
+    const setup = () => {
+      setIsSticking(false)
+      // Set filler height
+      const headerHeight = headerRef.current.getBoundingClientRect().height
+      headerOgHeight.current = headerHeight
+      fillerRef.current.style.height = headerHeight + "px"
+    }
+    setup()
+    addEventListener("resize", setup)
+    return () => removeEventListener("resize", setup)
   }, [headerRef, fillerRef])
 
   // calculating how much we've scrolled past the og header
+  // using min max to avoid unnecessary re-renders
+  // re-renders only when scrollPastHeader is between 0 and headerOgHeight
   const scrollPastHeader = useTransform(() =>
-    Math.max(scrollY.get() - headerOgHeight.current, 0),
+    Math.max(
+      Math.min(scrollY.get() - headerOgHeight.current, headerOgHeight.current),
+      0,
+    ),
   )
-
-  useMotionValueEvent(scrollPastHeader, "change", (latest) => {
-    // set sticking state if we've scrolled past the og header
-    setIsSticking(latest > 0)
-  })
 
   useEffect(() => {
     if (!isSticking) return
@@ -65,9 +75,14 @@ const Header = () => {
 
   const stickyHeaderY = useTransform(
     scrollPastHeader,
-    [0, headerShrunkenHeight.current],
-    [-headerShrunkenHeight.current, 0],
+    [0, headerShrunkenHeight.current + margin],
+    [-(headerShrunkenHeight.current + margin), 0],
   )
+
+  useMotionValueEvent(scrollPastHeader, "change", (latest) => {
+    // set sticking state if we've scrolled past the og header
+    setIsSticking(latest > 0)
+  })
 
   const stickyHeaderSpringY = useSpring(stickyHeaderY, {
     mass: 0.3,
@@ -108,87 +123,11 @@ const Header = () => {
     <MainMenuContext.Provider value={mainMenu}>
       <HeaderMenuContext.Provider value={headerMenu}>
         <StickyHeaderContext.Provider value={isSticking}>
-          {/* <div
+          <div ref={fillerRef} className="filler bg-[#003da6]"></div>
+          <motion.section
             id="header"
             ref={headerRef}
-            className="group/header relative z-50"
-          >
-            {/* <section className="mb-0 bg-theme text-white lg:pb-2 lg:pt-4">
-              <div className="flex flex-col">
-                <HeaderMenu />
-                <div className="relative h-16">
-                  <motion.div
-                    ref={stickyHeaderRef}
-                    style={{
-                      position: isSticking ? "fixed" : "absolute",
-                      top: 0,
-                      y: isSticking ? stickyHeaderSpringY : 0,
-                    }}
-                    className={`inset-x-0 z-[9999] grid h-16 place-items-center shadow-lg transition-shadow ${
-                      isSticking
-                        ? "bg-theme shadow-black/30 duration-1000"
-                        : "shadow-transparent duration-0"
-                    }`}
-                  >
-                    <div className="container flex flex-wrap items-center gap-x-4">
-                      <Desktop>
-                        <SiteLogo />
-                      </Desktop>
-                      <Mobile>
-                        <AnimatePresence>
-                          {!isSticking && <SiteLogo />}
-                          {isSticking && (
-                            <div className="w-1 grow">
-                              <SearchBar
-                                layoutId="search-bar"
-                                key="search-bar"
-                              />
-                            </div>
-                          )}
-                        </AnimatePresence>
-                      </Mobile>
-                      <Desktop>
-                        <div className="w-full lg:w-96">
-                          <SearchBar key="search-bar" />
-                        </div>
-                      </Desktop>
-
-                      <HeaderToolBar />
-                    </div>
-                  </motion.div>
-                </div>
-              </div>
-            </section> 
-
-            <a href="#pinned-product" className="skip">
-              Skip to main content
-            </a>
-
-            <section className="mb-0 bg-[#003da6] py-2 text-white">
-              <div className="container">
-                <Mobile>
-                  <AnimatePresence>
-                    {!isSticking ? (
-                      <div className="w-full lg:w-96">
-                        <SearchBar layoutId="search-bar" />
-                      </div>
-                    ) : (
-                      <div className="h-10"></div>
-                    )}
-                  </AnimatePresence>
-                </Mobile>
-                <Desktop>
-                  <MainMenuDesktop />
-                </Desktop>
-              </div>
-            </section>
-          </div> */}
-          {/* <div className="mb-12"></div> */}
-          <div ref={fillerRef} className="filler"></div>
-          <motion.div
-            id="header"
-            ref={headerRef}
-            className={`relative z-50 grid grid-cols-[1fr_repeat(12,_minmax(max-content,_1fr))_1fr] gap-x-2 bg-theme text-white ${
+            className={`relative z-50 grid w-full grid-cols-[1fr_repeat(12,_minmax(max-content,_1fr))_1fr] gap-x-2 bg-theme text-white ${
               isSticking
                 ? "grid-rows-[repeat(1,_min-content)]"
                 : "grid-rows-[repeat(3,_min-content)]"
@@ -199,8 +138,8 @@ const Header = () => {
               y: isSticking ? stickyHeaderSpringY : 0,
             }}
           >
-            <section
-              className={`container col-[2/-2] col-start-2 row-span-full mb-0 grid grid-cols-[subgrid] grid-rows-[subgrid] items-center xl:gap-x-4 `}
+            <div
+              className={`container col-[2/-2] row-span-full mb-0 grid grid-cols-[subgrid] grid-rows-[subgrid] items-center px-0 lg:gap-x-4 `}
             >
               <div
                 className={`absolute inset-0 col-[1/-1] row-[-2/-1] bg-[#003da6] ${
@@ -209,46 +148,45 @@ const Header = () => {
               ></div>
               <HeaderMenu
                 className={`col-[2/14] row-span-1 flex-wrap justify-end gap-3 py-2 ${
-                  isSticking ? "hidden" : "hidden xl:flex"
+                  isSticking ? "hidden" : "hidden lg:flex"
                 }`}
               />
 
               <SiteLogo
                 className={`-translate-y-1 items-end gap-1 py-3 ${
                   isSticking
-                    ? "col-[1/2] row-[1/2] hidden xl:flex"
+                    ? "col-[1/2] row-[1/2] hidden lg:flex"
                     : "col-[1/2] row-[2/3] flex"
                 }`}
               />
 
               <SearchBar
-                layoutId="search-bar"
                 className={`my-2 ${
                   isSticking
-                    ? "col-[1/11] row-[3/4] xl:col-[2/6] xl:row-[1/2]"
-                    : "col-[1/-1] row-[3/4] xl:col-[2/6] xl:row-[2/3]"
+                    ? "col-[1/10] row-[3/4] lg:col-[2/6] lg:row-[1/2]"
+                    : "col-[1/-1] row-[3/4] lg:col-[2/6] lg:row-[2/3]"
                 }`}
               />
 
               <HeaderToolBar
                 className={`ms-auto ${
                   isSticking
-                    ? "col-[11/13] row-[3/4] xl:row-[1/2]"
-                    : "col-[11/13] row-[2/3]"
+                    ? "col-[10/-1] row-[3/4] lg:row-[1/2] xl:col-[3/-1]"
+                    : "col-[3/-1] row-[2/3] lg:col-[9/-1]"
                 }`}
               />
 
               <MainMenuDesktop
                 className={`col-[1/-1] row-[3/4] flex-wrap gap-2 py-2 ${
-                  isSticking ? "hidden" : "hidden xl:flex"
+                  isSticking ? "hidden" : "hidden lg:flex"
                 }`}
               />
-            </section>
+            </div>
 
             <a href="#pinned-product" className="skip">
               Skip to main content
             </a>
-          </motion.div>
+          </motion.section>
         </StickyHeaderContext.Provider>
       </HeaderMenuContext.Provider>
     </MainMenuContext.Provider>
